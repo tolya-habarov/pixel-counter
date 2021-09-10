cfb20b37a57309fa0ea03d441565b3f91a4f3606
tolya <tolya.maximum@gmail.com>
2021-09-10 15:11:57 +0700

    Services refactoring.

--- app/services.py
+++ app/services.py
@@ -5,25 +5,23 @@ from typing import Tuple
 
 def get_pixel_count(
     file_path: str,
-    white_threshold: int=255,
-    black_threshold: int=0
+    white_pixel: Tuple[int, int, int]=(255, 255, 255),
+    black_pixel: Tuple[int, int, int]=(0, 0, 0),
     ) -> Tuple[int, int]:
     """Return bright and black pixel count
 
     Args:
         file_path (str): Path to image file
-        bright_threshold (int, optional): Bright pixel threshold. Defaults to 255.
-        black_threshold (int, optional): Black pixel threshold. Defaults to 0.
+        bright_pixel (int, optional): White pixel. Defaults to (255, 255, 255).
+        black_pixel (int, optional): Black pixel. Defaults to (0, 0, 0).
 
     Returns:
         Tuple[int, int]: Bright, black pixel count
     """
 
-    img = _open_image(file_path)
-
-    np_img = np.array(img)
-    white_count = np.sum(np_img >= white_threshold)
-    black_count = np.sum(np_img <= black_threshold)
+    np_img = _open_image(file_path)
+    white_count = _pixel_count_by_color(np_img, white_pixel)
+    black_count = _pixel_count_by_color(np_img, black_pixel)
 
     return white_count, black_count
 
@@ -39,19 +37,26 @@ def pixel_count_by_hex(file_path: str, hex_color: str) -> int:
         int: Pixel count
     """
 
-    h = hex_color.lstrip('#')
-    rgb_color = tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
-    
-    img = _open_image(file_path)
-    return np.sum(np.array(img) == rgb_color)
+    np_img = _open_image(file_path)
+    rgb_color = _hex_to_rgb(hex_color)
+    return _pixel_count_by_color(np_img, rgb_color)
 
 
-def _open_image(file_path: str):
+def _open_image(file_path: str) -> np.ndarray:
     img = Image.open(file_path)
     if img.mode == 'RGBA':
         img.load()
         background = Image.new("RGB", img.size, (255, 255, 255))
         background.paste(img, mask=img.split()[3])
-        return background
+        img = background
 
-    return img
+    return np.array(img)
+
+
+def _pixel_count_by_color(np_img: np.ndarray, rgb_color: Tuple[int, int, int]) -> int:
+    return round(np.sum(np_img == rgb_color) / 3) # rgb have 3 channel
+
+
+def _hex_to_rgb(hex_color: str) -> Tuple[int, int, int]:
+    hex = hex_color.lstrip('#')
+    return tuple(int(hex[i:i+2], 16) for i in (0, 2, 4))
